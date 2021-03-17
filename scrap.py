17e0b670a5ec9f225e164324324b6e564130415b
MysteriousBaboon <mysteriousbaboon@hotmail.com>
2021-03-17 20:04:38 +0100

    Added multi threading

--- scrap.py
+++ scrap.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import threading
+import time
 
 # for categories
 domain = "https://fr.trustpilot.com"
@@ -12,6 +14,34 @@ category_urls = ["food_beverages_tobacco", "animals_pets", "money_insurance",
                  "utilities", "home_services", "business_services", "legal_services_government",
                  "public_local_services", "shopping_fashion", "sports", "travel_vacation",
                  "vehicles_transportation"]
+
+
+def scrape_page(url):
+    global api_df
+    url = domain + url["href"]
+    page_2 = requests.get(url)
+    soup_2 = BeautifulSoup(page_2.text, 'lxml')
+
+    comp_name = soup_2.find("span", {"class": "multi-size-header__big"}).text
+    reviews = soup_2.find_all("div", {"class": "review-content"})
+
+    for el in reviews:
+        stars = el.find("div", {"class": "star-rating star-rating--medium"}).find('img')['alt'][0]
+        text = el.find("p", {"class": "review-content__text"})
+
+        try:
+
+            date = el.find("p", {"class": "review-content__dateOfExperience"}).text
+        except:
+            date = "too far"
+        try:
+            text = text.text
+        except:
+            text = None
+
+        ret = {"company": comp_name, "date": date, 'review': text, "note": stars}
+
+        api_df = api_df.append(ret, ignore_index=True)
 
 
 def scrap_db():
@@ -54,40 +84,32 @@ def scrap_db():
 
 
 def scrap_api(field: str, location: str):
-    df = pd.DataFrame(columns=['company', 'date', 'review', 'note'])
+    start_time = time.time()
+
+    global api_df
+    api_df = pd.DataFrame(columns=['company', 'date', 'review', 'note'])
 
     page = requests.get(domain + under_domain + field + f"?location={location}")
+
     soup = BeautifulSoup(page.text, 'lxml')
+
     company_urls = soup.find_all("a", {"class": "link_internal__YpiJI link_wrapper__LEdx5",
                                      "target": "_blank",
                                      "aria-label": "",
                                      "rel": ""})
-    for company in company_urls:
-        url = domain + company["href"]
-        page_2 = requests.get(url)
-        soup_2 = BeautifulSoup(page_2.text, 'lxml')
 
-        comp_name = soup_2.find("span", {"class": "multi-size-header__big"}).text
-        reviews = soup_2.find_all("div", {"class": "review-content"})
+    threads = [threading.Thread(target=scrape_page,args=(url,)) for url in company_urls]
+    for t in threads:
+        t.start()
 
-        for el in reviews:
-            stars = el.find("div", {"class": "star-rating star-rating--medium"}).find('img')['alt'][0]
-            text = el.find("p", {"class": "review-content__text"})
+    for t in threads:
+        t.join()
 
-            try:
 
-                date = el.find("p", {"class": "review-content__dateOfExperience"}).text
-            except:
-                date = "too far"
-            try:
-                text = text.text
-            except:
-                text = None
-
-            df = df.append({"company": comp_name,
-                            "date": date, 'review': text, "note": stars}, ignore_index=True)
 
         #TODO df = pd.DataFrame(columns=['company', 'category', 'global_note', 'number_reviews', 'date', '])
+    print(time.time() - start_time)
+    print(api_df)
+    return api_df
 
-    return df
-
+scrap_api("sports", "paris")
