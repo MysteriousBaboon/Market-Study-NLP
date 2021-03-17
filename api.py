7b9cd8cddcea61b001c9f88fc0ff22c33ae1a78e
franck_loic22 <franck.loic22@gmail.com>
2021-03-17 16:27:05 +0100

    return json

--- api.py
+++ api.py
@@ -1,18 +1,24 @@
 from flask import Flask, request, escape
 import scrap
+import json
+import matplotlib.pyplot
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
 def home_page():
-  return 'welcome'
+  return 'welcome to the best API'
 
 @app.route('/locality=<local>/domain=<domain>/')
 def search(local, domain):
-    print(scrap.scrap_api(domain, local))
-    return local + domain
+    df = scrap.scrap_api(domain, local)
+    result = df.to_json(orient='split')
+    parse = json.loads(result)
+    test = json.dumps(parse, indent=4)
+    open('data.json', 'w').write(str(test))
+    return str(test)
 
-search('paris', 'sports')
+
 
 app.run(host='127.0.0.1', port=5000)
