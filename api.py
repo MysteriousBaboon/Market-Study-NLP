3c5e79bff5c39ce289c3d95521a958636a9a6443
franck-loic22 <77440382+franck-loic22@users.noreply.github.com>
2021-03-18 16:14:19 +0100

    final json with jsonify

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import scrap
 import json
 import NLP
@@ -9,20 +9,52 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def home_page():
-    return 'welcome to the best API'
+    return jsonify(
+            wordcloud=
+                    {'recent':
+                        {
+                        'pos':3,
+                        'neg':6
+                        },
+                    'old':
+                        {
+                        'pos':0,
+                        'neg':4,
+                        }
+                    },
+            no_reviews=0,
+            no_companies=1,
+                    )
 
 
 @app.route('/locality=<local>/domain=<domain>/')
 def search(local, domain):
 
     scrap_df = scrap.scrap_api(domain, local)
-
     treated_df = NLP.predict(scrap_df)
-    result = treated_df.to_json(orient='split')
-    parse = json.loads(result)
-    test = json.dumps(parse, indent=4)
 
-    return str(test)
+    # df_recent = treated_df[treated_df[date]]
+
+    no_reviews = int(treated_df['review'].count())
+    no_companies = int(treated_df['company'].nunique())
+
+
+    return jsonify(
+            wordcloud=
+                    {'recent':
+                        {
+                        'pos':3,
+                        'neg':6
+                        },
+                    'old':
+                        {
+                        'pos':0,
+                        'neg':4,
+                        }
+                    },
+            no_reviews=no_reviews,
+            no_companies=no_companies
+                   )
 
 
 app.run(host='127.0.0.1', port=5000)
